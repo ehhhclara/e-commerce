e1d446a9d9c6cb848930ec11bd7c9b1b63bf1c66
ehhhclara <mfailache@fcien.edu.uy>
2022-10-27 23:36:36 -0300

    cambios provisorios del carrito

--- js/cart.js
+++ js/cart.js
@@ -1,6 +1,13 @@
 const usuario25801 = `https://japceibal.github.io/emercado-api/user_cart/25801.json`;
 let cartProd ={};
 
+let totalFinal = document.getElementById("totalId");
+let subtotalFinal = document.getElementById("costoUnitarioId");
+let precioEnvio = document.getElementById("costoEnvioId");
+let envioS = document.getElementById("envioSta");
+let envioE = document.getElementById("envioExp");
+let envioP = document.getElementById("envioPre");
+
 
 
 
@@ -14,10 +21,33 @@ for ( i = 0; i < product.length; i++) {
     <td id="precioUnitario">${product[i].unitCost}</td>
     <td><input  id="inputCantidad" onclick="subTotal()" type="number" value="${product[i].count}" min="1"/></td>
     <td class="col-2" id="precioTotal">${product[i].unitCost}</td>`
+       
+//sección costos
+
+
+    if (envioP.checked){
+      precioEnvio.innerHTML = `USD ${(product[i].unitCost * 0.15)}`;
+    subtotalFinal.innerHTML=`USD ${(product[i].unitCost)}`
+    totalFinal.innerHTML = `USD ${(product[i].unitCost * 0.15)+(product[i].unitCost)}`
+    ;}
+
+     else if (envioE.checked){
+        precioEnvio.innerHTML = `USD ${(product[i].unitCost * 0.07)}`;
+        subtotalFinal.innerHTML=`USD ${(product[i].unitCost)}`
+        totalFinal.innerHTML = `USD ${(product[i].unitCost * 0.07)+(product[i].unitCost)}`
+        ;}
+        
+       
+       else if (envioS.checked){
+          precioEnvio.innerHTML = `USD ${(product[i].unitCost * 0.05)}`;
+          subtotalFinal.innerHTML=`USD ${(product[i].unitCost)}`
+          totalFinal.innerHTML = `USD ${(product[i].unitCost * 0.05)+(product[i].unitCost)}`
+          ;}
+
+
+      };}
+
       
-      }
-      
-    };
 
     function subTotal(){
         let cantidad = document.getElementById("inputCantidad").value;
@@ -25,6 +55,8 @@ for ( i = 0; i < product.length; i++) {
         let preciotxt = precio.innerText;
         let precioFinal = document.getElementById("precioTotal");
         precioFinal.innerHTML = cantidad * parseInt(preciotxt);
+
+  
     };
 
 
