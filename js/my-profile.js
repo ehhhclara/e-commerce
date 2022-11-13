d11d3a2969da493a7faf506d82e26f87af9398bf
ehhhclara <mfailache@fcien.edu.uy>
2022-11-12 23:01:01 -0300

    Entrega 7 validaciones corregidas

--- js/my-profile.js
+++ js/my-profile.js
@@ -1,14 +1,32 @@
 let infoDelUsuario = {};
 const user = JSON.parse(localStorage.getItem('mailLocal'));
 const boton = document.getElementById("boton-sbm")
+let msjError = document.getElementById("mensajeError")
+const infoDUsuario = JSON.parse(localStorage.getItem('infoDelUsuario'));
 
 
 document.addEventListener("DOMContentLoaded", () => {
     document.getElementById("emailInp").value = user.mail;})
 
 
-    boton.addEventListener("click", () => {
 
+
+//validaciones
+    boton.addEventListener("click", () =>{
+if(`${document.getElementById("primerNombreInp").value}`=== '' )
+    {msjError.innerHTML = '<p style="color: red">Debe ingresar el nombre</p>'}
+    else if(`${document.getElementById("primerApellidoInp").value}`=== '' )
+    {msjError.innerHTML = '<p style="color: red">Debe ingresar el apellido</p>'}
+    else if(user.mail=== '' )
+    {msjError.innerHTML = '<p style="color: red">Debe ingresar el mail</p>'}
+
+else 
+{ msjError.innerHTML ='<p style="color: green">Datos actualizados correctamente</p>'};
+});
+
+
+// guardar elementos en el local storage
+    boton.addEventListener("click", () => {
     infoDelUsuario = {
         primerNombre: `${document.getElementById("primerNombreInp").value}`,
         segundoNombre: `${document.getElementById("segundoNombreInp").value}`,
@@ -17,19 +35,19 @@ document.addEventListener("DOMContentLoaded", () => {
         telefono: `${document.getElementById("telContactoInp").value}`,
     };
     localStorage.setItem("infoDelUsuario",JSON.stringify(infoDelUsuario));
-});
-
-console.log(localStorage)
-const infoDUsuario = JSON.parse(localStorage.getItem('infoDelUsuario'));
-console.log(infoDUsuario)
+}
+);
 
 
+// mostrar elementos en el formulario
 addEventListener("DOMContentLoaded", () => {
+    
     {
             document.getElementById("primerNombreInp").value = infoDUsuario.primerNombre;
             document.getElementById("segundoNombreInp").value = infoDUsuario.segundoNombre;
             document.getElementById("primerApellidoInp").value = infoDUsuario.primerApellido;
             document.getElementById("segundoApellidoInp").value = infoDUsuario.segundoApellido;
             document.getElementById("telContactoInp").value = infoDUsuario.telefono;
-        }}
-        )
+        }
+}
+        );
